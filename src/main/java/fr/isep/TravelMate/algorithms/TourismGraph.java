324b98822846f8c9b1fba27dfb9a85ad228cab73
jbouveret18 <jbouveret18@gmail.com>
2023-06-05 13:54:35 +0200

    Error fixed

--- src/main/java/fr/isep/TravelMate/algorithms/TourismGraph.java
+++ src/main/java/fr/isep/TravelMate/algorithms/TourismGraph.java
@@ -3,74 +3,96 @@ package fr.isep.TravelMate.algorithms;
 import java.util.*;
 
 public class TourismGraph {
-    private List<TourismAttraction> attractions;
-    private Map<TourismAttraction, Map<TourismAttraction, Double>> adjacencyMap;
+    private Map<TourismAttraction, List<GraphEdge>> graph;
 
     public TourismGraph() {
-        attractions = new ArrayList<>();
-        adjacencyMap = new HashMap<>();
+        this.graph = new HashMap<>();
     }
 
     public void addAttraction(TourismAttraction attraction) {
-        attractions.add(attraction);
-        adjacencyMap.put(attraction, new HashMap<>());
+        graph.put(attraction, new ArrayList<>());
     }
 
-    public void addEdge(TourismAttraction attraction1, TourismAttraction attraction2) {
-        double distance = attraction1.calculateDistance(attraction2);
-        adjacencyMap.get(attraction1).put(attraction2, distance);
-        adjacencyMap.get(attraction2).put(attraction1, distance);
+    public void addEdge(TourismAttraction source, TourismAttraction destination, double distance) {
+        graph.get(source).add(new GraphEdge(destination, distance));
+        graph.get(destination).add(new GraphEdge(source, distance));
     }
 
     public List<TourismAttraction> findClosestAttraction(TourismAttraction source) {
-        Map<TourismAttraction, Double> distanceMap = new HashMap<>();
-        Map<TourismAttraction, TourismAttraction> previousMap = new HashMap<>();
+        Map<TourismAttraction, Double> distances = new HashMap<>();
+        Map<TourismAttraction, TourismAttraction> previous = new HashMap<>();
+        Set<TourismAttraction> visited = new HashSet<>();
 
-        Set<TourismAttraction> unvisited = new HashSet<>(attractions);
+        PriorityQueue<AttractionDistance> queue = new PriorityQueue<>(Comparator.comparingDouble(AttractionDistance::getDistance));
+        queue.add(new AttractionDistance(source, 0.0));
 
-        // Set initial distances to infinity except for the source attraction
-        for (TourismAttraction attraction : attractions) {
-            distanceMap.put(attraction, Double.POSITIVE_INFINITY);
-        }
-        distanceMap.put(source, 0.0);
+        distances.put(source, 0.0);
 
-        while (!unvisited.isEmpty()) {
-            TourismAttraction current = getClosestAttraction(distanceMap, unvisited);
-            unvisited.remove(current);
+        while (!queue.isEmpty()) {
+            AttractionDistance current = queue.poll();
+            TourismAttraction currentAttraction = current.getAttraction();
 
-            for (TourismAttraction neighbor : adjacencyMap.get(current).keySet()) {
-                double distance = distanceMap.get(current) + adjacencyMap.get(current).get(neighbor);
-                if (distance < distanceMap.get(neighbor)) {
-                    distanceMap.put(neighbor, distance);
-                    previousMap.put(neighbor, current);
+            if (visited.contains(currentAttraction)) {
+                continue;
+            }
+
+            visited.add(currentAttraction);
+
+            for (GraphEdge edge : graph.get(currentAttraction)) {
+                TourismAttraction neighbor = edge.getDestination();
+                double distance = edge.getDistance();
+                double totalDistance = distances.get(currentAttraction) + distance;
+
+                if (!distances.containsKey(neighbor) || totalDistance < distances.get(neighbor)) {
+                    distances.put(neighbor, totalDistance);
+                    previous.put(neighbor, currentAttraction);
+                    queue.add(new AttractionDistance(neighbor, totalDistance));
                 }
             }
         }
 
-        List<TourismAttraction> path = new ArrayList<>();
-        TourismAttraction attraction = source;
+        List<TourismAttraction> closestAttractions = new ArrayList<>();
 
-        while (previousMap.containsKey(attraction)) {
-            path.add(attraction);
-            attraction = previousMap.get(attraction);
+        for (TourismAttraction attraction : distances.keySet()) {
+            closestAttractions.add(attraction);
         }
 
-        Collections.reverse(path);
-        return path;
+        return closestAttractions;
     }
 
-    private TourismAttraction getClosestAttraction(Map<TourismAttraction, Double> distanceMap, Set<TourismAttraction> unvisited) {
-        TourismAttraction closestAttraction = null;
-        double minDistance = Double.POSITIVE_INFINITY;
+    private static class GraphEdge {
+        private final TourismAttraction destination;
+        private final double distance;
 
-        for (TourismAttraction attraction : unvisited) {
-            double distance = distanceMap.get(attraction);
-            if (distance < minDistance) {
-                closestAttraction = attraction;
-                minDistance = distance;
-            }
+        public GraphEdge(TourismAttraction destination, double distance) {
+            this.destination = destination;
+            this.distance = distance;
         }
 
-        return closestAttraction;
+        public TourismAttraction getDestination() {
+            return destination;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+    }
+
+    private static class AttractionDistance {
+        private final TourismAttraction attraction;
+        private final double distance;
+
+        public AttractionDistance(TourismAttraction attraction, double distance) {
+            this.attraction = attraction;
+            this.distance = distance;
+        }
+
+        public TourismAttraction getAttraction() {
+            return attraction;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
     }
 }
