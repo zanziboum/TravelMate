588a06605c5a28013bec7fe3f006333c9d50cbc7
jbouveret18 <jbouveret18@gmail.com>
2023-06-05 13:32:57 +0200

    getter usage

--- src/main/java/fr/isep/TravelMate/algorithms/TourismAttraction.java
+++ src/main/java/fr/isep/TravelMate/algorithms/TourismAttraction.java
@@ -11,39 +11,24 @@ public class TourismAttraction {
         this.longitude = longitude;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
 
     public double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-
     // Calculate the distance between two attractions using the Haversine formula
     public double calculateDistance(TourismAttraction other) {
         double earthRadius = 6371; // in kilometers
 
-        double lat1Rad = Math.toRadians(this.latitude);
+        double lat1Rad = Math.toRadians(getLatitude());
         double lat2Rad = Math.toRadians(other.latitude);
-        double deltaLat = Math.toRadians(other.latitude - this.latitude);
-        double deltaLong = Math.toRadians(other.longitude - this.longitude);
+        double deltaLat = Math.toRadians(other.latitude - getLatitude());
+        double deltaLong = Math.toRadians(other.longitude - getLongitude());
 
         double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
                 + Math.cos(lat1Rad) * Math.cos(lat2Rad)
