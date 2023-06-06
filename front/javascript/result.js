006ba69ba49226898c95d10059c1ab31f785a202
zanziboum <zanziboum.zanzi@orange.fr>
2023-06-06 23:51:32 +0200

    Integration de la partie Recherche itineraire

--- front/javascript/result.js
+++ front/javascript/result.js
@@ -15,13 +15,85 @@ form.addEventListener('submit', function(event) {
 
     // Affichez les valeurs dans la div des résultats
     const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = `
-            <p>Options sélectionnées: ${selectedOptions}</p>
-            <p>Départ: ${departVille}, ${departLieu}</p>
-            <p>Arrivée: ${arriveeVille}, ${arriveeLieu}</p>
-            <p>Valeur du slider: ${sliderValue}</p>
-        `;
+
+    const types = getSelectedTypes()
+
+    console.log(types)
+
+
+    let param = new URLSearchParams();
+    param.append("start", departLieu);
+    param.append("end", arriveeLieu);
+
+    param.append("nbNode",sliderValue)
+
+    if (types.length === 0){
+        axios.get(`${apiUrl}/PathNoFilter`, { params: param })
+            .then(response => {
+                const attractions = response.data;
+                console.log(response)
+                showResult(response,resultsDiv)
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des attractions :', error);
+            });
+    }else{
+        types.forEach(type => param.append("kinds", type));
+        axios.get(`${apiUrl}/PathKindFilter`, { params: param })
+            .then(response => {
+                const attractions = response.data;
+                console.log(response)
+                showResult(response,resultsDiv)
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des attractions :', error);
+            });
+    }
+
 
     // Réinitialisez le formulaire (facultatif)
-    form.reset();
+    //form.reset();
 });
+
+function getSelectedTypes(){
+    var selectedOptionsDiv = document.getElementById('selected-options');
+    var selectedOptions = selectedOptionsDiv.getElementsByClassName('selected-option');
+    var typesBatiments = [];
+
+    for (var i = 0; i < selectedOptions.length; i++) {
+        var selectedOption = selectedOptions[i];
+        var typeBatiment = selectedOption.childNodes[0].nodeValue.trim();
+        typesBatiments.push(typeBatiment);
+    }
+    return typesBatiments
+}
+
+function showResult(response,resultsDiv){
+    const attractions = response.data;
+
+    // Effacez le contenu précédent de resultsDiv
+    resultsDiv.innerHTML = '';
+
+    // Parcourez les attractions et créez des éléments HTML pour chaque attraction
+    attractions.forEach(attraction => {
+        // Créez un élément de div pour l'encadré de l'attraction
+        const attractionDiv = document.createElement('div');
+        attractionDiv.classList.add('attraction');
+
+        // Créez un élément de titre pour le nom de l'attraction
+        const attractionName = document.createElement('h3');
+        attractionName.textContent = attraction.name;
+        attractionDiv.appendChild(attractionName);
+
+        // Créez un élément de paragraphe pour d'autres informations sur l'attraction (score, etc.)
+        const attractionInfo = document.createElement('p');
+        attractionInfo.textContent = `Ville: ${attraction.city.name} `;
+        const attractionInfo2 = document.createElement('p');
+        attractionInfo2.textContent = `Score: ${attraction.score}`;
+        attractionDiv.appendChild(attractionInfo);
+        attractionDiv.appendChild(attractionInfo2);
+
+        // Ajoutez l'encadré de l'attraction à resultsDiv
+        resultsDiv.appendChild(attractionDiv);
+    });
+}
--- src/main/java/fr/isep/TravelMate/controller/TourismController.java
+++ src/main/java/fr/isep/TravelMate/controller/TourismController.java
@@ -85,5 +85,4 @@ public class TourismController {
         return tourismGraphService.pathWithKindFilter(start,end,nbNode,kinds);
     }
 
-
 }
